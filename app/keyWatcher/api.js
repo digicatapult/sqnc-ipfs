2bd89b11f38e23a5acecbcd10e03ec728006ca7e
Jonathan Gray <jonmattgray@gmail.com>
2022-11-28 14:04:10 +0000

    use polkadot directly

--- app/keyWatcher/api.js
+++ app/keyWatcher/api.js
@@ -1,20 +1,18 @@
-import { buildApi } from '@digicatapult/dscp-node'
+import { ApiPromise, WsProvider, Keyring } from '@polkadot/api'
 
 import env from '../env.js'
 
 export const createNodeApi = async () => {
-  const { api, keyring } = buildApi({
-    options: {
-      apiHost: env.NODE_HOST,
-      apiPort: env.NODE_PORT,
-    },
-  })
+  const provider = new WsProvider(`ws://${env.NODE_HOST}:${env.NODE_PORT}`)
+  const api = new ApiPromise({ provider })
+
+  api.isReadyOrError.catch(() => {}) // prevent unhandled promise rejection errors
 
   await api.isReady
 
   return {
     _api: api,
-    _keyring: keyring,
+    _keyring: new Keyring({ type: 'sr25519' }),
     isEventKeyUpdate: (event) => api.events.ipfsKey.UpdateKey.is(event),
     getCurrentKey: async () => await api.query.ipfsKey.key(),
     setupEventProcessor: (eventProcessor) => api.query.system.events(eventProcessor),
