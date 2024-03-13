d532a7072be8ee7be0019438e19e44bc78bc12d8
Matthew Dean <matthew.dean@digicatapult.org.uk>
2024-03-13 12:46:11 +0000

    Bug/investigate startup issue logging (#192)
    * Add additional logging on startup error
    
    * 2.10.10
    
    * increase test timeout
    
    * Increase logging in testing
    
    * Revert "Increase logging in testing"
    
    This reverts commit 727a963f2368a8e828cd8dfd0daac141c59c70e3.

--- app/keyWatcher/api.js
+++ app/keyWatcher/api.js
@@ -1,12 +1,16 @@
 import { ApiPromise, WsProvider, Keyring } from '@polkadot/api'
 
+import logger from '../logger.js'
 import env from '../env.js'
 
 export const createNodeApi = async () => {
   const provider = new WsProvider(`ws://${env.NODE_HOST}:${env.NODE_PORT}`)
   const api = new ApiPromise({ provider })
 
-  await api.isReadyOrError.catch(() => {}) // prevent unhandled promise rejection errors
+  await api.isReadyOrError.catch((e) => {
+    logger.error('Error thrown initialising API, message: %s', e?.message)
+    logger.debug('Error thrown initialising API, stack: %j', e?.stack)
+  }) // prevent unhandled promise rejection errors
 
   return {
     _api: api,
--- test/mocharc.cjs
+++ test/mocharc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   recursive: true,
   slow: 10000,
-  timeout: 20000,
+  timeout: 30000,
   extension: ['.test.js'],
   require: ['./test/integration/fixtures.js'],
   exit: true,
